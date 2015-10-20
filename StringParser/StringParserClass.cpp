e66e3e3a5e2521e84972dc5f5d36e4f02deb728d
JacobB5 <Jake.Bechtel.5@gmail.com>
2015-10-20 12:13:14 -0400

    Update

--- StringParser/StringParserClass.cpp
+++ StringParser/StringParserClass.cpp
@@ -19,13 +19,13 @@ StringParserClass::StringParserClass(void)
 	pStartTag=0;
 	pEndTag=0;
 	areTagsSet=false;
-	lastError=SUCCEEDED;
+	lastError=ERROR_NO_ERROR;
 }
 
 int StringParserClass::getLastError()
 {
 	int tempError=lastError;
-	lastError=SUCCEEDED;
+	lastError=ERROR_NO_ERROR;
 	return tempError;
 }
 
@@ -48,8 +48,21 @@ bool StringParserClass::setTags(const char *pStartTag, const char *pEndTag)
 
 bool getDataBetweenTags(char *pDataToSearchThru, vector<string> &myVector)
 {
-	if(areTagsSet){
 
+	if (!pDataToSearchThru){
+		lastError = ERROR_DATA_NULL;
+		return false;
+	}
+	else{
+		if(!areTagsSet){
+			return false;
+		}
+		else{
+			//DO NOT INCLUDE THE TAGS THEMSELVES
+			// USE findtag to checck
+
+
+		}
 	}
 
 
@@ -57,6 +70,64 @@ bool getDataBetweenTags(char *pDataToSearchThru, vector<string> &myVector)
 
 bool findTag(char *pTagToLookFor, char *&pStart, char *&pEnd)
 {
-	
+
+}
+
+
+
+
+//Example
+void convertStringtoCharPointer(){
+	//Shows how to convert a string to a char pointer
+	std::string myString="12345";
+	int len = myString.length();
+	int lenPlusNull=len+1; //add one for terminating null
+	char *p=new char[lenPlusNull];
+	//set memory aside and write to 0's which also places a terminating null
+	memset(p,0,lenPlusNull*sizeof(char));
+
+	//Copy from the string to p, 5 chars
+	myString.copy(p,len,0);
+	//p now has a copy of the string in myString
+
+	//Iterate over the pointer and set all values to 'a'
+	for(int i=0;i<len;i++)
+		*(p+1)='a';
+
+	if(p){
+		delete[] p;
+		p=0;
+	}
+}
+
+
+//A simple search for a single item
+void charBasedStringComparison(char* p){
+
+	if(!p){
+		return;
+	}
+
+	//how long in tag
+	int iten_P=strlen(p);
+
+	char myString[] = "01234456";
+	char *pinc = &myString[0];
+
+	int iLen_MyString = strlen(pinc)/sizeof(char);
+
+	int compare=SUCCEEDED;
+
+	for(int ctr =0;ctr<iLen_MyString;ctr++){
+		if (*p == *(pinc + ctr)){
+			//Compare function start at p, go through to (pinc+ctr), but stop after iLen_p chars)
+			compare = memcmp(p,(pinc+ctr),iten_P);
+			if (compare == SUCCEEDED){
+				std::cout<<"Found it"<<endl;
+				return;
+			}
+		}
+
+	}
 }
 
