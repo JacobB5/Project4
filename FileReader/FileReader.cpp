7ec96b2802d488cc0b5166b0b6f917bbc9dca582
Nigel Armstrong <legonigel@gmail.com>
2015-10-22 03:04:21 -0400

    File reading code cleanup
    ifstream does not throw exceptions (unless you explicitly ask it to)
    that is why we check for good() and open(). I removed the unnecessary
    try-catch blocks.
    Also ran visual studio's formatter over the file (CTRL + K + D)

--- FileReader/FileReader.cpp
+++ FileReader/FileReader.cpp
@@ -4,25 +4,25 @@
 #include "..\Include\constants.h"
 #include "..\Include\Debug_Help.h"
 using namespace std;
-namespace KP_FileReaderClass{
+namespace KP_FileReaderClass {
 
 	//TODO Fill this in
-	FileReader::FileReader(){
-		filecontents="";
+	FileReader::FileReader() {
+		filecontents = "";
 	}
 
 	FileReader::~FileReader() {
-		
+
 	}
 
 
 	int FileReader::getFileContents(const std::string filename, std::string &contents)
 	{
-		int returnCode=ReadTheWholeFile(filename);
-		if (returnCode==SUCCEEDED){
-			contents=filecontents;
+		int returnCode = ReadTheWholeFile(filename);
+		if (returnCode == SUCCEEDED) {
+			contents = filecontents;
 		}
-		return returnCode;	
+		return returnCode;
 	}
 
 	int FileReader::ReadTheWholeFile(const std::string &filename)
@@ -31,38 +31,22 @@ namespace KP_FileReaderClass{
 
 		if (myInputFile.good())
 		{
-			try
-			{
+			myInputFile.open(filename.c_str(), ios::in);
 
-				myInputFile.open(filename.c_str(), ios::in);
+			if (myInputFile.is_open()) {
+				string line;
 
-				if (myInputFile.is_open()){				
-					string line;
-
-					while (!myInputFile.eof()) {
-						getline(myInputFile, line);
-						filecontents+=line;
-					}
-					try{
-						myInputFile.close();
-						return SUCCEEDED;
-					}
-					catch(exception& e)
-					{
-						cout<<"Exception occured: "<< e.what() << endl;
-						return COULD_NOT_CLOSE_FILE;
-					}
+				while (!myInputFile.eof()) {
+					getline(myInputFile, line);
+					filecontents += line;
 				}
-
+				myInputFile.close();
+				return SUCCEEDED;
 			}
-			catch (exception& e)
-			{
 
-				cout<<"Exception occured: "<< e.what() << endl;
-				return COULD_NOT_OPEN_FILE;
-			}
 		}
-		else return COULD_NOT_OPEN_FILE;
+		
+		return COULD_NOT_OPEN_FILE;
 	}
 
 }
